f22c091c368a3e0735ec9ef7d4136db23fee9a26
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2023-01-05 02:34:32 +0530

    Added few view functions with placeholder views

--- DAA/Q/views.py
+++ DAA/Q/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Start your assessment!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at result of question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
