4fdbd6b7157e4199d22045c3eb03c86ef5954003
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2023-01-04 23:28:40 +0530

    Added string magic method for Questions class to print the question text while object is called.

--- DAA/Q/models.py
+++ DAA/Q/models.py
@@ -5,6 +5,9 @@ class Questions(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     questions = models.ForeignKey(Questions, on_delete=models.CASCADE)
