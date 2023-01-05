da284f9b74377e80874de05fdfac36e68ca8a738
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2023-01-06 01:43:41 +0530

    Added the new login view

--- DAA/Q/views.py
+++ DAA/Q/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import Question
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Question, Userdata
 import json
 
 
+@csrf_exempt
 def index(request):
     question = list(Question.objects.values())
-    data = {}
     for q in question:
         qes_obj = Question.objects.get(pk=int(q['id']))
         choices = list(qes_obj.choice_set.values())
@@ -16,4 +18,12 @@ def index(request):
         q['name'] = 'Q' + str(q['id'])
     data = json.dumps(question)
     return render(request, "Q/index.html", {'data': data})
-    return HttpResponse("Hi")
+
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        user_data = json.loads(request.body)
+        u = Userdata(username=user_data['username'], response_list=user_data['data'])
+        u.save()
+    return render(request, "Q/login.html")
